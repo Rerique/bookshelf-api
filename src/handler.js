971e7f1ea12ab6309cf12150ddf2d849b9240bdb
Rerique <efpri.nanda@gmail.com>
2023-01-05 16:41:33 +0700

    menambahkan query parameter

--- src/handler.js
+++ src/handler.js
@@ -74,16 +74,77 @@ const addBook = (request, h) => {
   return response;
 };
 
-const getAllBooks = () => ({
-  status: 'success',
-  data: {
-    books: books.map((book) => ({
-      id: book.id,
-      name: book.name,
-      publisher: book.publisher,
-    })),
-  },
-});
+const getAllBooks = (request, h) => {
+  const { name, reading, finished } = request.query;
+  console.table(request.query);
+
+  if (name !== undefined) {
+    const filterName = books.filter((book) =>
+      book.name.toLowerCase().includes(name.toLowerCase())
+    );
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: filterName.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (reading !== undefined) {
+    const filterReading = books.filter(
+      (book) => Number(book.reading) === Number(reading)
+    );
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: filterReading.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  if (finished !== undefined) {
+    const filterFinished = books.filter(
+      (book) => Number(book.finished) === Number(finished)
+    );
+    const response = h.response({
+      status: 'success',
+      data: {
+        books: filterFinished.map((book) => ({
+          id: book.id,
+          name: book.name,
+          publisher: book.publisher,
+        })),
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: books.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
+  return response;
+};
 
 const getBookById = (request, h) => {
   const { bookId } = request.params;
